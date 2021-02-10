3ea30c7579d86ced7439c36b3fc36a001b76307a
Jeremias ramirez <jereramirez499@gmail.com>
2021-02-09 23:12:25 -0400

    creado banner card

--- src/app/components/icon/icon.component.ts
+++ src/app/components/icon/icon.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input} from '@angular/core';
 
 @Component({
   selector: 'app-icon',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./icon.component.css']
 })
 export class IconComponent implements OnInit {
-
+  @Input() iconRoute:string = '';
   constructor() { }
 
   ngOnInit(): void {
