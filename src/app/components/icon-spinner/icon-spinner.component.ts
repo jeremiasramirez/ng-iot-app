ebbdb08cf6cab586007406aaa77ba0124f396fe7
Jeremias ramirez <jereramirez499@gmail.com>
2021-02-11 20:05:33 -0400

    creando spinner para home load

--- src/app/components/icon-spinner/icon-spinner.component.ts
+++ src/app/components/icon-spinner/icon-spinner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-icon-spinner',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./icon-spinner.component.css']
 })
 export class IconSpinnerComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
